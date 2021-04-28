7cccd6a7802229b42c5ba6f4f460a05743047a42
Matheus Ferreira <matheusfdantas03@gmail.com>
2021-04-28 15:33:49 -0300

    Estrutura da aplicação, Componentização, Consumindo API

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   :root {
     --red: #E52E40;
+    --green: #33CC95;
     --blue: #5429CC;
     
     --blue-light: #6933FF;
